8bfa6fd4c1acbaa5f02cdcc7b4d4663e9957c55c
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-06 10:44:53 +0200

    Add files via upload

---
+++ JavaCore 0501 Making a cat/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+/*
+
+0501 Создание кота
+Создать класс Main. У Main должно быть имя (name, String), возраст (age, int), вес (weight, int), сила (strength, int).
+
+Требования:
+1. Программа не должна считывать данные с клавиатуры.
+2. У класса Main должна быть переменная name с типом String.
+3. У класса Main должна быть переменная age с типом int.
+4. У класса Main должна быть переменная weight с типом int.
+5. У класса Main должна быть переменная strength с типом int.
+6. Всего в классе должно быть 4 переменных.
+
+*/
+
+public class Main {
+    String name;
+    int age;
+    int weight;
+    int strength;
+    public static void main(String[] args) throws Exception {
+    }
+}
+
+
+
+
